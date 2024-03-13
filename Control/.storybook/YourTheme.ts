f86ab13df39757f0a00059c1fff18a9190c4a09e
asllanmakaj7 <animakaj7@gmail.com>
2024-03-13 16:11:16 +0100

    Updated to Storybook 8

---
+++ Control/.storybook/YourTheme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+  brandTitle: "ORBIS.PCF.ColorfulOptionset",
+  brandUrl: "https://github.com/ORBISAG/ORBIS.PCF.ColorfulOptionset",
+  brandImage: "./Shko Online 92x32.svg",
+  brandTarget: "_self",
+});
--- Control/.storybook/main.js
+++ Control/.storybook/main.js
@@ -9,29 +9,31 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    '@storybook/addon-webpack5-compiler-babel'
   ],
   framework: {
     name: '@storybook/html-webpack5',
     options: {},
-},
-webpackFinal: async (config) => {
-  config.resolve.alias = config.resolve.alias || {};
-  config.resolve.alias['react'] = path.resolve('./node_modules/react');
-  config.resolve.alias['react-dom'] = path.resolve('./node_modules/react-dom');
-  config.resolve.fallback = config.resolve.fallback || {};
-  config.resolve.fallback['react/jsx-dev-runtime'] = path.resolve('./.storybook/react-jsx-dev-runtime.development.js');
-  config.resolve.fallback['react/jsx-runtime'] = path.resolve('./.storybook/react-jsx-runtime.production.min.js');
+  },
+  staticDirs: ['./public'],
+  webpackFinal: async (config) => {
+    config.resolve.alias = config.resolve.alias || {};
+    config.resolve.alias['react'] = path.resolve('./node_modules/react');
+    config.resolve.alias['react-dom'] = path.resolve('./node_modules/react-dom');
+    config.resolve.fallback = config.resolve.fallback || {};
+    config.resolve.fallback['react/jsx-dev-runtime'] = path.resolve('./.storybook/react-jsx-dev-runtime.development.js');
+    config.resolve.fallback['react/jsx-runtime'] = path.resolve('./.storybook/react-jsx-runtime.production.min.js');
 
-  config.plugins.push(
+    config.plugins.push(
       new webpack.SourceMapDevToolPlugin({
-          append: '\n//# sourceMappingURL=[url]',
-          fileContext: './',
-          filename: '[file].map',
+        append: '\n//# sourceMappingURL=[url]',
+        fileContext: './',
+        filename: '[file].map',
       }),
-  );
- 
+    );
 
-  return config;
-},
+
+    return config;
+  },
 }
